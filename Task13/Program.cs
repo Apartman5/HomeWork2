4f7ab45470ee19421e2bef0cb039a10a71313784
Igor Kazakov <kiw@lefterova.ru>
2023-03-04 17:55:44 +0500

    first commit

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: 
+// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// Примеры:
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите любое целое число: ");
+string digits = Console.ReadLine();
+int number = Convert.ToInt32(digits);
+if(number < 100)
+{
+    Console.WriteLine("Третьей цифры в числе нет!");
+}
+else 
+{
+    Console.WriteLine($"Третья цифра числа c конца - {digits[digits.Length-3]}");
+    Console.WriteLine($"Третья цифра числа c начала - {digits[2]}");
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 15: 
+// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// Примеры:
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Input the serial number of the day of the week:");
+int weekDay = Convert.ToInt32(Console.ReadLine());
+
+bool WeekEnd(int number)
+{
+    if (number > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekday(int day)
+{
+    if (day > 0 && day < 8)
+    {
+        return true;
+    }
+    Console.WriteLine("It`s not a day of the week!");
+    return false;
+}
+
+if(ValidateWeekday(weekDay))
+{
+    if(WeekEnd(weekDay))
+    {
+        Console.WriteLine("It`s a weekend!");
+    }
+    else Console.WriteLine("It`s not a weekend!");
+}
